18a30d99fc5da19f4b9c7113fc9abb1b389af302
swati srivastava <swatisrivastava91189@gmail.com>
2021-07-19 16:36:45 +0530

    committing changes to login

--- src/test/java/webDriverLoginGitHub/LoginTest.java
+++ src/test/java/webDriverLoginGitHub/LoginTest.java
@@ -1,5 +1,7 @@
 package webDriverLoginGitHub;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -25,6 +27,7 @@ public class LoginTest {
 	public void doLogin() throws InterruptedException {
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElement(By.id("ap_email")).sendKeys("918448602082");
 		driver.findElement(By.id("continue")).click();
 		driver.findElement(By.id("ap_password")).sendKeys("@swati*91189#");
